110091278d9894577a21886ddf0c72c931280f56
unknown <toghrul@code.edu.az>
2022-08-04 18:29:08 +0400

    .

---
+++ task25.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int radius = int.Parse(Console.ReadLine());
+            Console.WriteLine(2);
+
+        }
+        static void areaOfCircle(int radius)
+        {
+            Console.WriteLine(3 * radius * radius);
+
+            {
+
+
+            }
+            
+
+            
+            
+        
+        
+    
+
